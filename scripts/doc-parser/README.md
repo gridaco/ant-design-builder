1372ccb2982d9f6cd97ef84cbf467a01d2ff879f
softmarshmallow <woojoo@softmarshmallow.com>
2021-01-15 14:46:55 +0900

    added oktokit as deps

---
+++ scripts/doc-parser/README.md
@@ -0,0 +1,2 @@
+# Ant design doc parser
+> this script parses desiered documents, and code snippets from github repository, and the ant-d official website.
--- scripts/doc-parser/index.ts
+++ scripts/doc-parser/index.ts
@@ -3,7 +3,6 @@ import Axios from "axios";
 var xpath = require('xpath')
     , dom = require('xmldom').DOMParser
 
-import { Parameter } from "coli/lib"
 
 const axios = Axios.create(
     {
